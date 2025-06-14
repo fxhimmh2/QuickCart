02aca69e72482c4d3cd72c0057e447cade92b1b2
fxhimmh2 <fahimhowladermh@gmail.com>
2025-06-14 16:41:15 +0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
-<<<<<<< HEAD
 const userSchema = new mongoose.Schema({
     _id:{ type: String, required:true},
     name:{ type: String, required:true},
@@ -8,15 +7,6 @@ const userSchema = new mongoose.Schema({
     imageUrl:{ type: String, required:true},
     cartItems:{ type: Object, default: {} }
 }, {minimize: false })
-=======
-const userSchema = new mongoose.Schema ({
-    _id:{ type : String, required:true},
-    name: { type : String, required:true},
-    email: { type : String, required:true, unique:true },
-    imageUrl: { type : String, required:true},
-    cartItems: { type : Object, default : {}}
-}, {minimize:false })
->>>>>>> 12bc9695e2b1dbe74023db9cccda84ed0a16594a
 
 const User = mongoose.models.user || mongoose.model('user', userSchema)
 
